9b0e83f668f5774c1095f30ec07f4d2f55ffea06
SulfurPT <76445413+SulfurPT@users.noreply.github.com>
2021-04-19 22:51:54 +0100

    Update exercio01.py

--- exercio01.py
+++ exercio01.py
@@ -1,10 +1,10 @@
 pessoas = int(input('Quantas pessoas tem na loja? '))
 
-if pessoas < 5 
+if pessoas < 5:
     print('o risco é Baixo)
-    elif pessoas >= 5 and pessoas <= 10
-        print ('o risco é Medio)
-elif pessoas >= 11 and pessoas < 15
+elif pessoas >= 5 and pessoas <= 10:
+    print ('o risco é Medio)
+elif pessoas >= 11 and pessoas < 15:
     print ('o risco é Elevado)
-    else
-        print('o risto é Extremo)
+else:
+    print('o risto é Extremo)
